14224551cea6428f7fd1e45ac44b346f720d45e9
Leonardo <140932426+leontwas@users.noreply.github.com>
2025-06-05 17:14:29 -0300

    revierto cambios

--- assets/js/productosData.js
+++ assets/js/productosData.js
@@ -6,83 +6,83 @@ export const productos = [
     "PC Gaming-city",
     "Pc Marca Gaming-city HO-0C0S16D-5700G-VEGA8 Amd Ryzen 7 5700g 8 Nucleos, 16gb De Ram, 480gb SSD",
     652287,
-    "../images/img1.png"
+    "./assets/images/img1.png"
   ),
   new Producto(
     2,
     "Mouse Gamer T-dagger",
     "Mouse Gamer T-dagger Imperial T-tgm310rgb Black, sensor óptico de 7200 Dpi, Iluminación RGB",
     22243,
-    "../images/img2.png"
+    "./assets/images/img2.png"
   ),
   new Producto(
     3,
     "Notebook Lenovo V15 G4 Iru",
     "Notebook Lenovo V15 G4 Iru, Intel Core I5 13420h, 40gb De Ram Ddr4, 512gb Ssd M.2 Pcie (4.0x4) Nvme",
     550000,
-    "../images/img3.png"
+    "./assets/images/img3.png"
   ),
   new Producto(
     4,
     "PC Compact Galaxy 10.5",
     'PC Compact Galaxy 10.5" 256gb, Intel Core I5 1135G7, 8gb De Ram, 120gb Ssd M.2 Pcie (4.0x4) Nvme',
     470999,
-    "../images/img4.png"
+    "./assets/images/img4.png"
   ),
   new Producto(
     5,
     "Auriculares Gamer JBL",
     "Auricular Gamer JBL Quantum 100MVE Signature - Conector jack 3.5 mm (TRRS) - Sonido de 2400 hz",
     59999,
-    "../images/img5.png"
+    "./assets/images/img5.png"
   ),
   new Producto(
     6,
     "Tablet Lenovo Tab M10",
     'Tablet Lenovo Tab M10 3rd Gen TB328FU 10" 64GB 4GB RAM 2K 5000mah Wifi Bluetooth',
     283499,
-    "../images/img6.png"
+    "./assets/images/img6.png"
   ),
   new Producto(
     7,
     "Notebook HP 15FC0A",
     "Notebook HP 15FC0A Color Plateado 8gb De Ram - 256gb Ssd - Amd Ryzen 5 - Windows 11 instalado.",
     621199,
-    "../images/img7.png"
+    "./assets/images/img7.png"
   ),
   new Producto(
     8,
     "Monitor + Teclado y mouse",
     'Combo de monitor LED 19" + teclado y mouse inalámbricos Etheos - Conexión USB (plug & play) - No incluye CPU.',
     307999,
-    "../images/img8.png"
+    "./assets/images/img8.png"
   ),
   new Producto(
     9,
     "PC TechMaster",
     "Pc TechMaster TM-0BS16I Intel Core i7- 8 Núcleos, 16GB de RAM, 512GB SSD, Tarjeta Gráfica Nvidia GTX 1660",
     757999,
-    "../images/img9.png"
+    "./assets/images/img9.png"
   ),
   new Producto(
     10,
     "PC Gamer AMD Ryzen 5",
     "PC HP -Gamer  AMD Ryzen 5 5600G - 16GB RAM - SSD 480GB - Radeon Vega 7 - WiFi - HDMI",
     789999,
-    "../images/img10.png"
+    "./assets/images/img10.png"
   ),
   new Producto(
     11,
     "Impresora HP DeskJet Ink Advantage 2775",
     "Impresora multifunción HP DeskJet Ink Advantage 2775 color - escáner y copiadora.",
     274999,
-    "../images/img11.png"
+    "./assets/images/img11.png"
   ),
   new Producto(
     12,
     "Notebook Lenovo Ideapad Slim",
     "Notebook Lenovo Ideapad Slim 3 - Ryzen 3 - 8 GB RAM - 256 GB SSD - Windows 11 HOME.",
    699999,
-    "../images/img12.png"
+    "./assets/images/img12.png"
   )
 ];
