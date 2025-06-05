c6de493850c56dbbc220528a54ed6d9659d1a956
Leonardo <140932426+leontwas@users.noreply.github.com>
2025-06-05 16:37:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const connection = require("./database");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,17 +11,6 @@ app.use(express.json());
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, "public")));
-
-// Ruta para obtener usuarios desde la base de datos
-app.get("/usuarios", (req, res) => {
-  connection.query("SELECT * FROM usuario", (err, results) => {
-    if (err) {
-      console.error("Error al obtener los usuarios:", err);
-      return res.status(500).json({ error: "Error al obtener los usuarios" });
-    }
-    res.json(results);
-  });
-});
 
 // Para cualquier otra ruta, devolver el archivo index.html
 app.get("/*", (req, res) => {
